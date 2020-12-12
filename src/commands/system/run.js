36364a8c15fb28dbdd81e03e6a1742ccfd12498d
3vil <heinjosang@gmail.com>
2020-12-12 03:13:07 +0100

    Added run command and settings in message

---
+++ src/commands/system/run.js
@@ -0,0 +1,25 @@
+const { MessageEmbed } = require("discord.js");
+
+exports.run = async (client, message, args) => {
+  return Promise.resolve().then(async () => {
+    try{
+      // Eval the code
+      let e = await eval(args.join(' '))
+      if(typeof(e) == 'object') e = JSON.stringify(e)
+      else if(typeof(e) != 'string') e = e.toString()
+      message.channel.send(new MessageEmbed({description: e}))
+    }catch(e){
+      client.log('err', e)
+    }
+  })
+};
+
+
+exports.conf = {
+    aliases: ['r'],
+};
+
+exports.help = {
+    name: "run",
+    description: `Evals code`,
+};
--- src/events/message.js
+++ src/events/message.js
@@ -1,6 +1,13 @@
+const Settings = require('../models/Settings')
 module.exports = async (client, message) => {
     if(message.author.bot) return;
     if(!message.content.startsWith(client.prefix)) return;
+    if(message.guild && !client.settings.has((message.guild||{}).id)) {
+        // We don't have the settings for this guild, generate empty settings
+        let s = (await Settings.findOneAndUpdate({_id:"default"}, {}, {upsert:true, setDefaultsOnInsert:true, new:true})).toObject()
+        client.settings.set(message.guild.id, s)
+        message.settings = s
+    }
     // Do whatever message filtering here
 
     let args = message.content.substring(client.prefix.length).split(' ')
